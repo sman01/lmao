fb55b1b483a0a2c1719f94ada5888e141954f22c
sman01 <shreemun@gmail.com>
2020-12-13 20:57:13 +0530

    Combiner added, finally done

---
+++ combine.py
@@ -0,0 +1,12 @@
+
+import os
+import glob
+import pandas as pd
+os.chdir(r"C:\Users\shree\OneDrive\Desktop\ScrapeU\reviews")
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
+#export to csv
+combined_csv.to_csv( "Review.csv", index=False, encoding='utf-8-sig')
+print("all done")
