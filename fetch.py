790b3ae0d6daf065a76d94113216fc5af365b965
sman01 <shreemun@gmail.com>
2021-01-04 19:48:58 +0530

    updated user agent

--- fetch.py
+++ fetch.py
@@ -26,7 +26,7 @@ def err(url,no):
             
     else:
         return 3
-headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0'}
+headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'}
 
 reve=0
 
