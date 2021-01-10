89f8dcf35fdc7f9b450d96d4078c50bd1e18ad3f
sman01 <shreemun@gmail.com>
2021-01-10 14:25:42 +0530

    combine.py print some extra metadata

--- combine.py
+++ combine.py
@@ -2,9 +2,11 @@
 import os
 import glob
 import pandas as pd
-os.chdir(r"/home/sman/Desktop/lmao/rev2/")
+os.chdir(r"/home/sman/Desktop/lmao/reviews/")
 extension = 'csv'
 all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+print(type(all_filenames))
+print(all_filenames)
 #combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
 #export to csv
