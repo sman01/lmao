a23b848b15388268d25dab7be71a752d9cec9434
sman01 <shreemun@gmail.com>
2021-01-26 13:13:59 +0530

    hahaha

--- combine.py
+++ combine.py
@@ -7,8 +7,6 @@ extension = "csv"
 all_filenames = [i for i in glob.glob("*.{}".format(extension))]
 print(type(all_filenames))
 all_filenames.sort()
-print(all_filenames)
-print(len(all_filenames))
 
 # combine all files in the list
 combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
