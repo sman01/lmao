9c7cd3caaa05407fcaca2638ebf7e817ac9c0cc3
sman01 <shreemun@gmail.com>
2021-01-26 13:09:03 +0530

    owfn

--- combine.py
+++ combine.py
@@ -1,17 +1,17 @@
-
 import os
 import glob
 import pandas as pd
+
 os.chdir(r"/home/sman/Desktop/raflao/sman/review/")
-extension = 'csv'
-all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+extension = "csv"
+all_filenames = [i for i in glob.glob("*.{}".format(extension))]
 print(type(all_filenames))
 all_filenames.sort()
 print(all_filenames)
 print(len(all_filenames))
 
-#combine all files in the list
-combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-#export to csv
-combined_csv.to_csv( "Final_Review.csv", index=False, encoding='utf-8-sig')
+# combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+# export to csv
+combined_csv.to_csv("Final_Review.csv", index=False, encoding="utf-8-sig")
 print("all done")
